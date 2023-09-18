797aefdcaded5a3275c5c6b92f97c9514f310885
Shiranezu <aweleoluonye@gmail.com>
2023-09-18 17:41:55 +0100

    kkk

--- python_class.py
+++ python_class.py
@@ -1,4 +1,6 @@
-age= input('get users age')
-
-if age > 18:
-    print('hahahaha, get younger')
+def check(age):
+    if age > 18:
+        print('hahahaha, get younger')
+        
+age= input('get users age: ')   
+check(age)
